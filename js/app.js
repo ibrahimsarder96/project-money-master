53f04f466b8ee75e690475c821be364c8cee6ed9
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-02-17 17:02:42 +0600

    four commit

--- js/app.js
+++ js/app.js
@@ -13,3 +13,30 @@ function getInputField(num1, num2, num3){
   const inputIncomeField = document.getElementById('total-income');
   const inputText = inputIncomeField.value;
   const totalIncome = parseFloat(inputText);
+  // function call
+  const foodCost = getInputField('food-cost');
+  const rentCost = getInputField('rent-cost');
+  const clothCost = getInputField('cloth-cost');
+  //calculation
+  let totalCostAmount = foodCost + rentCost + clothCost;
+  let balance = totalIncome - totalCostAmount;
+  // update total
+  const totalCost = document.getElementById('total-cost');
+  if(totalIncome < totalCostAmount){ 
+  alert('not enough money')
+  }
+  if((foodCost > 0) || ( rentCost > 0) || (clothCost > 0)){
+  const totalText = totalCost.innerText;
+  totalCost.innerText = totalCostAmount;
+  }
+  else{
+  alert('please, give a number value')
+  }
+  //update balance
+  const totalBalance = document.getElementById('total-balance');
+
+  if((foodCost > 0) || ( rentCost > 0) || (clothCost > 0)){
+  const balanceText = totalBalance.innerText;
+  totalBalance.innerText = balance;
+  }
+})
